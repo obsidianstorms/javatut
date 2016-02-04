df8df00c7e4989870c44a6a228ec3a505a5c0e95
Rick Yust <ryust@efolder.net>
2016-02-03 21:04:38 -0700

    Chapter 3

---
+++ hf-java/ch03/BooksTestDrive.java
@@ -0,0 +1,26 @@
+class BooksTestDrive {
+    public static void main(String[] args) {
+        //declare the variable AND create the array of sized 3
+        Books[] myBooks = new Books[3];
+        int x = 0;
+
+        //have to actually fill the array slots w/ objects to reference
+        myBooks[0] = new Books();
+        myBooks[1] = new Books();
+        myBooks[2] = new Books();
+
+        myBooks[0].title = "The Grapes of Java";
+        myBooks[1].title = "The Java Gatsby";
+        myBooks[2].title = "The Java Cookbook";
+        myBooks[0].author = "bob";
+        myBooks[1].author = "sue";
+        myBooks[2].author = "ian";
+
+        while (x < 3) {
+            System.out.print(myBooks[x].title);
+            System.out.print(" by ");
+            System.out.println(myBooks[x].author);
+            x = x + 1;
+        }
+    }
+}
